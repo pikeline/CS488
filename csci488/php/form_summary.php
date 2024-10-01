3a791d3e6e7ca9241b655f08a23c99c4b48d1ec0
Dennis Cherepanov <dennis@cherepanov.org>
2024-10-01 13:03:35 -0500

    More hw

--- csci488/php/form_summary.php
+++ csci488/php/form_summary.php
@@ -1,5 +1,6 @@
 <?php
 $submitted_form_data = array_merge($_GET, $_POST);
+$order = array("name", "email", "age", "winner", "fear", "arena", "favorite", "factor", "confidence", "explanation", "hidden");//order of php array, change this to change order of table
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,30 +15,34 @@ $submitted_form_data = array_merge($_GET, $_POST);
     <table>
         <th>
             <tr>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Age</td>
-                <td>Winner</td>
-                <td>Scariness</td>
-                <td>Arena</td>
-                <td>Favorite</td>
-                <td>Most important factor</td>
-                <td>Confidence</td>
-                <td>Why</td>
-                <td>Hidden</td>
+                <?php foreach ($order as $key) { ?>
+                    <td><?php echo ucwords($key) ?></td>
+                <?php } ?>
             </tr>
         </th>
         <tr>
             <td><?php echo $submitted_form_data['name'] ?></td>
             <td><?php echo $submitted_form_data['email'] ?></td>
             <td><?php echo $submitted_form_data['age'] ?></td>
-            <td><?php echo $submitted_form_data['winner'] ?></td>
-            <td><?php echo $submitted_form_data['scariness'] ?></td>
+            <td><?php
+            if (isset($submitted_form_data['winner'])) {
+                foreach ($submitted_form_data['winner'] as $winner) {
+                    echo $winner . "<br>";
+                }
+            }
+            ?></td>
+            <td><?php echo $submitted_form_data['fear'] ?></td>
             <td><?php echo $submitted_form_data['arena'] ?></td>
-            <td><?php echo $submitted_form_data['favorite'] ?></td>
-            <td><?php echo $submitted_form_data['most_important_factor'] ?></td>
+            <td><?php
+            if (isset($submitted_form_data['favorite'])) {
+                foreach ($submitted_form_data['favorite'] as $favorite) {
+                    echo $favorite . "<br>";
+                }
+            }
+            ?></td>
+            <td><?php echo $submitted_form_data['factor'] ?></td>
             <td><?php echo $submitted_form_data['confidence'] ?></td>
-            <td><?php echo $submitted_form_data['why'] ?></td>
+            <td><?php echo $submitted_form_data['explanation'] ?></td>
             <td><?php echo $submitted_form_data['hidden'] ?></td>
     </table>
 </body>
